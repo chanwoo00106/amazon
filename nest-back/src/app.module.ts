9ac264d2c505d2667adb72261b5f93b7ca879c51
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-11-28 20:13:23 +0900

    config auth

--- nest-back/src/app.module.ts
+++ nest-back/src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/amazon'),
     ProductModule,
+    UserModule,
+    AuthModule,
   ],
   providers: [AppService],
 })
