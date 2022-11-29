9dc901e83ae12f3eaef1a97a52c23c8205eebed5
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-11-29 23:09:06 +0900

    Add page settting

---
+++ front/src/utils/themes.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff9900',
+      contrastText: '#000'
+    },
+    secondary: {
+      main: '#000',
+      contrastText: '#fff'
+    }
+  }
+})
